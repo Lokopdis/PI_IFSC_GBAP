eb35ad29f134b6a4061e6c8f97e7232f4ac02263
Lokopdis <eduardo.turcatto2001@gmail.com>
2024-07-11 17:24:55 -0300

    Update main.cpp

--- PI_IV_GBAP_OPERACIONAL/src/main.cpp
+++ PI_IV_GBAP_OPERACIONAL/src/main.cpp
@@ -2,6 +2,10 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+///////////////////////// DIRETRIZES //////////////////////////
+#define INTERRUPCAO false
+#define TESTE true
+
 /////////////////////////// DEFINES ///////////////////////////
 // LÓGICA DE ACIONAMENTO
 #define Pino_Botao_Emergencia 15
@@ -10,6 +14,25 @@
 
 // ACIONAMENTO REMOTO
 #define Pino_Acionamento_Remoto 33 // Alterar
+
+// MOTOR 1 ----->
+// ENCODER
+#define Encoder_Canal_A_Motor_1 5 // Canala A do encoder do motor 1
+#define Encoder_Canal_B_Motor_1 17 // Canal B do encoder do motor 1
+
+// SAÍDA ANALÓGICA
+#define Pino_Motor_1 25 // Saída analógica para o motor 1
+
+// MOTOR 2 <-----
+//ENCODER
+#define Encoder_Canal_A_Motor_2 4 // Canal A do encoder do motor 2
+#define Encoder_Canal_B_Motor_2 2 // Canal B do encoder do motor 2
+
+// SAÍDA ANALÓGICA
+#define Pino_Motor_2 26 // Saída de analógica para o motor 2
+
+// GERAL
+#define TAXA_AMOSTRAGEM 100 // Valor em ms
 
 ////////////////////////// VARIAVEIS //////////////////////////
 // LÓGICA DE ACIONAMENTO
@@ -20,12 +43,56 @@ bool Robo_Ok = false; // Variável de acionamento do robô
 // ACIONAMENTO REMOTO
 bool Status_Remoto = false;
 
+// ENCODER GERAL
+const double Pulsos_Por_Revolucao_Encoder = 1000; // Número de pulsos por revolução do encoder
+const double Reducao_Encoder_Motor = 3; // Redução entre encoder e eixo do motor
+const double Reducao_Motor_Roda = 32; // Rudação entre eixo do motor e roda
+
+// MOTOR 1 ----->
+// ENCODER 
+volatile long Contador_Pulsos_Canal_A_Motor_1 = 0; // Contador de pulsos do canal A do encoder do motor 1
+volatile long Contador_Pulsos_Canal_B_Motor_1 = 0; // Contador de pulsos do canal B do encoder do motor 1
+
+// PWM
+int PWM_Motor_1 = 3103; // Valor inicial para velocidade do motor 1
+
+// RPS
+double RPS_Motor_1 = 0; // Valor em m/s do motor 1
+long Aux_Convert_PWM_Motor_1 = 0; // Variável auxiliar para conversão do valor do PWM de 10 p/ 8 bits
+
+// MOTOR 2 <-----
+// ENCODER 
+volatile long Contador_Pulsos_Canal_A_Motor_2 = 0; // Contador de pulsos do canal A do encoder do motor 2
+volatile long Contador_Pulsos_Canal_B_Motor_2 = 0; // Contador de pulsos do canal B do encoder do motor 2
+
+// PWM
+int PWM_Motor_2 = 3103; // Valor inicial para velocidade do motor 2
+
+// RPS
+double RPS_Motor_2 = 0; // Valor em m/s do motor 2
+long Aux_Convert_PWM_Motor_2 = 0; // Variável auxiliar para conversão do valor do PWM de 10 p/ 8 bits
+
+// CONTROLE
+
 //////////////////// DECLARAÇÃO DE FUNÇÕES ///////////////////
+// SETUP INICIAL 
+void SetupInicial ();
+
 // LÓGICA DE ACIONAMENTO
 void StatusAcionamento();
 
 // ACIONAMENTO REMOTO
 void AcionamentoRemoto();
+
+// MOTOR 1 ----->
+// ENCODER 
+void Incrementar_Pulsos_Canal_A_Motor_1();
+void Incrementar_Pulsos_Canal_B_Motor_1();
+
+// MOTOR 2 <-----
+// ENCODER 
+void Incrementar_Pulsos_Canal_A_Motor_2();
+void Incrementar_Pulsos_Canal_B_Motor_2();
 
 //////////////////////////// SETUP ///////////////////////////
 void setup() {
@@ -49,6 +116,22 @@ void setup() {
   // ACIONAMENTO REMOTO
   attachInterrupt(digitalPinToInterrupt(Pino_Acionamento_Remoto), AcionamentoRemoto, CHANGE);
 
+  // MOTOR 1 ----->
+  // ENCODER
+  attachInterrupt(digitalPinToInterrupt(Encoder_Canal_A_Motor_1), Incrementar_Pulsos_Canal_A_Motor_1, FALLING);
+  attachInterrupt(digitalPinToInterrupt(Encoder_Canal_B_Motor_1), Incrementar_Pulsos_Canal_B_Motor_1, FALLING);
+
+  // PWM
+  pinMode(Pino_Motor_1, OUTPUT); 
+
+  // MOTOR 2 <-----
+  // ENCODER
+  attachInterrupt(digitalPinToInterrupt(Encoder_Canal_A_Motor_2), Incrementar_Pulsos_Canal_A_Motor_2, FALLING);
+  attachInterrupt(digitalPinToInterrupt(Encoder_Canal_B_Motor_2), Incrementar_Pulsos_Canal_B_Motor_2, FALLING); 
+
+    // PWM
+  pinMode(Pino_Motor_1, OUTPUT);
+
 }
 
 /////////////////////////// LOOP ////////////////////////////
@@ -58,6 +141,11 @@ void loop() {
 }
 
 ///////////////////////// FUNÇÕES ///////////////////////////
+// SETUP INICIAL
+void SetupInicial(){
+
+}
+
 // LÓGICA DE ACIONAMENTO
 void StatusAcionamento (){
 
@@ -98,5 +186,24 @@ void AcionamentoRemoto(){
   }else{
     digitalWrite(Pino_Rele_Seguranca, HIGH);
   }
+}
 
+// MOTOR 1 ----->
+// ENCODER 
+void Incrementar_Pulsos_Canal_A_Motor_1(){
+  Contador_Pulsos_Canal_A_Motor_1++;
+}
+
+void Incrementar_Pulsos_Canal_B_Motor_1(){
+  Contador_Pulsos_Canal_B_Motor_1++;
+}
+
+// MOTOR 2 <-----
+// ENCODER 
+void Incrementar_Pulsos_Canal_A_Motor_2(){
+  Contador_Pulsos_Canal_A_Motor_2++;
+}
+
+void Incrementar_Pulsos_Canal_B_Motor_2(){
+  Contador_Pulsos_Canal_B_Motor_2++;
 }
